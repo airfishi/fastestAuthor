7a8d2cbda6553989189781759ef867158c93d7a4
airfishi <fishthesupreme@gmail.com>
2022-05-12 09:50:58 -0500

    testing

--- src/worldRecords/main.java
+++ src/worldRecords/main.java
@@ -11,13 +11,16 @@ public class main {
 		records.addBook(1992, "Jill","Flavortown");
 		records.addBook(1992, "Jill", "Megaville");
 		records.massAddBooks("2002.txt");
+		for(int i = 0; i < 40; i++) {
+			records.addBook(1476, "Bill" + i%7,"test" + i);
+		}
 		System.out.println(records.validYears());
 		
 		/*
 		 * The only date that will work currently are the ones you input 
-		 * since massAddBooks is not yet implemented.
+		 * since massAddBooks is not yet implemented.x
 		 */
-		System.out.printf("%s has the most books written for the date %d.\n",records.mostWritten(1992), 1992);
+		System.out.printf("%s has the most books written for the date %d.\n",records.mostWritten(1476), 1476);
 		
 		
 		
